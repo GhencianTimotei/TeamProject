4d0165ebf9a1a04f9ee1e351341c3b2c1e19217d
GhencianTimotei <timotei.ghencian00@e-uvt.ro>
2021-04-21 14:23:09 +0300

    Adding home_user and other changes

---
+++ Site/javascript/sidebars.js
@@ -0,0 +1,4 @@
+var dropDown = document.getElementById("dropdownUser1");
+dropDown.addEventListener('click', ev => {
+  document.getElementById("dropdown-menu").classList.toggle("dropdown-menu_show");
+});
